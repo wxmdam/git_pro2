92e8e0a624bc84878844e13138008432e7030e09
wxm <1984129971@qq.com>
2023-06-07 00:34:28 +0800

    idea master测试冲突

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("master");
         System.out.println("master2");
         System.out.println("master master");
+        System.out.println("master master master");
     }
 }
